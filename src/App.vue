7f3c8c0d33b032b762e7015c4cec5e60bbe12b35
VILLADIEGOCODE <carlos@fondarconsulting.com>
2023-10-08 12:31:45 -0500

    se agergo funciona con asyn y componente titulo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import BlogsPost from './components/BlogsPost.vue';
 import PaginatePost from './components/PaginatePost.vue';
 import LoadingComponent from './components/LoadingComponent.vue';
+import TitleComponent from './components/TitleComponent.vue';
 const posts = ref([]);
 const postXpage = 7;
 const inicio = ref(0);
 const fin = ref(postXpage);
 const loading = ref(true)
-
 const favorito = ref('')
 const addFavorito = (title) => {
   favorito.value = title;
@@ -24,23 +24,47 @@ const prev = ()=>{
   fin.value = fin.value - postXpage;
 }
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(res => res.json())
-.then((data) => {
-  posts.value = data;
-})
-.finally(()=> {
-  setTimeout(()=>{
-    loading.value = false
-  },800)
-});
+// onMounted(async()=>{
+//   try {
+//     const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     posts.value = await resp.json();
+    
+//   } catch (error) {
+//     console.log(error);
+    
+//   } finally{
+//     setTimeout(()=>{
+//       loading.value = false;
+//     },800)
+//   }
+ 
+// });
+//TODO: se pude realizar la petición sin el onMounted, ya que primero se realizaria la peticion y se tendria la data y despues se cargaria 
+//el componente, si se requiere acceder a un elemento del DOM antes de que cargue la data, entonces si se debe utilizar el onMounted()
+
+const getPosts = async ()=>{
+  try {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+    posts.value = await resp.json();
+    
+  } catch (error) {
+    console.log(error);
+    
+  }finally{
+    setTimeout(()=> (loading.value = false), 800 );
+  }
+
+}
+
+//Ejecuta la función de traer los posts
+getPosts();
 
 </script>
 
 <template>
    <LoadingComponent v-if="loading"/>
   <div class="container" v-else>
-    <h1>APP</h1>
+   <TitleComponent/>
     <h3>Favorito: <span class="fst-italic text-success">{{ favorito }}</span></h3>
     <PaginatePost @next="next" @prev="prev" :inicio="inicio" :fin="fin" :maxLength="posts.length"/>
     <BlogsPost v-for="post of posts.slice(inicio, fin)"
---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1 data-title="POSTSAPP" class="text">
+        POSTSAPP
+    </h1>
+</template>
+
+<style scoped>
+  .text{
+    color: transparent;
+    font-size: 3vw;
+    margin-top: 10px;
+    display: inline-block;
+    border-radius: 3px;
+    background-color: #1d3557;
+    position: relative;
+    -webkit-background-clip: text;
+    background-clip: text;
+    &:before{
+      content: attr(data-title);
+      background: linear-gradient(90deg, rgb(3, 159, 13) 0%, rgb(238, 176, 94) 51%, rgb(31, 61, 103) 100%);
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 0;
+      background-clip: text;
+      -webkit-background-clip: text;
+      transition: all 1s ease-in-out;
+      animation: text-animation 3s infinite;
+    }
+  }
+
+
+@keyframes text-animation{
+  0%{
+    width: 0;
+  }
+  50%{
+    width: 100%;
+  }
+  100%{
+    width: 0;
+  }
+}
+
+</style>
